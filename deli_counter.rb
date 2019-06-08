942c01fffe7aabd4c62111b95a194582f8c5ba5e
Abideen Kareem <abideenkareem17@gmail.com>
2019-06-08 15:50:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@
 def line(katz_deli)
   katz_deli[i] == " " ? "The line is currently empty." : 
   for i in katz_deli 
-  "Hi #{katz_deli}"
+  "Hi " katz_deli[i] ", you are " + (i+1) + " in line."
 end
 end 
