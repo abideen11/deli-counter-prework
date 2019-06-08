d9f9711c0bb192e1d8101082752e471d1e40d72d
Abideen Kareem <abideenkareem17@gmail.com>
2019-06-08 16:00:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli)
  if katz_deli == " "
-   "The line is currently empty"
+   "The line is currently empty."
  else 
    katz_deli.each do |i| 
      "Hi #{katz_deli}, you are " + (i+1) + " in line."
